ef6d8485a08c23b1072698782491b1d8f273de0e
lxl66566 <lxl66566@gmail.com>
2024-05-18 16:55:14 +0800

    20240518-16:55:14

--- backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-git-se e
-git commit -m $(date "+%Y%m%d-%H:%M:%S")
-git push git@github.com:lxl66566/diary.git main
-git push git@github.com:absxsfriends/diary.git main
---
+++ upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+git-se e
+git commit -m $(date "+%Y%m%d-%H:%M:%S")
+git push git@github.com:lxl66566/diary.git main
